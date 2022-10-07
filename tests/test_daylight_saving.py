7cf3d61b53415920e4a77b35663606f7cdd80668
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2022-10-07 18:48:27 +0000

    Added test of change to normal time.

--- tests/test_daylight_saving.py
+++ tests/test_daylight_saving.py
@@ -21,7 +21,14 @@ from tests.helpers.helpers import (
     MockSOCEntity,
     MockTargetSOCEntity,
 )
-from tests.price import PRICE_20220326, PRICE_20220327, PRICE_20220328
+from tests.price import (
+    PRICE_20220326,
+    PRICE_20220327,
+    PRICE_20220328,
+    PRICE_20221029,
+    PRICE_20221030,
+    PRICE_20221031,
+)
 from .const import MOCK_CONFIG_ALL
 
 # pylint: disable=unused-argument
@@ -139,6 +146,126 @@ async def test_to_daylight_saving_time(
         raw_schedule.get_value(
             datetime(
                 2022, 3, 28, 6, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        == 0
+    )
+
+
+async def test_from_daylight_saving_time(
+    hass: HomeAssistant, skip_service_calls, set_cet_timezone, freezer
+):
+    """Test Coordinator when going to daylight saving time"""
+
+    freezer.move_to("2022-10-29T14:00:00+02:00")  # Daylight saving time
+
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    MockSOCEntity.create(hass, entity_registry, "11")
+    MockTargetSOCEntity.create(hass, entity_registry, "80")
+    MockPriceEntity.create(hass, entity_registry, 10)
+    MockChargerEntity.create(hass, entity_registry, STATE_OFF)
+
+    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_ALL, entry_id="test")
+    coordinator = EVSmartChargingCoordinator(hass, config_entry)
+    assert coordinator
+
+    sensor: EVSmartChargingSensor = EVSmartChargingSensor(config_entry)
+    assert sensor is not None
+    await coordinator.add_sensor(sensor)
+    await coordinator.switch_active_update(True)
+    await coordinator.switch_apply_limit_update(True)
+
+    # Provide price
+    MockPriceEntity.set_state(hass, PRICE_20221029, PRICE_20221030)
+
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid
+
+    # This should give 12 hours of charging
+    # From UTC 2022-10-29 17:00 (19:00 CEST)
+    # To UTC 2022-10-30 05:00 (06:00 CET)
+
+    # pylint: disable=protected-access
+    raw_schedule: Raw = Raw(coordinator._charging_schedule)
+    assert raw_schedule.number_of_nonzero() == 12
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 29, 18, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        == 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 29, 19, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        != 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 30, 5, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        != 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 30, 6, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        == 0
+    )
+
+    freezer.move_to("2022-10-30T14:00:00+01:00")  # Normal time
+
+    # Provide price
+    MockPriceEntity.set_state(hass, PRICE_20221030, PRICE_20221031)
+
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid
+
+    # This should give 12 hours of charging
+    # From UTC 2022-10-30 17:00 (18:00 CET)
+    # To UTC 2022-10-31 05:00 (06:00 CEST)
+
+    # pylint: disable=protected-access
+    raw_schedule: Raw = Raw(coordinator._charging_schedule)
+    assert raw_schedule.number_of_nonzero() == 12
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 30, 17, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        == 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 30, 18, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        != 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 31, 5, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+            )
+        )
+        != 0
+    )
+    assert (
+        raw_schedule.get_value(
+            datetime(
+                2022, 10, 31, 6, 30, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
             )
         )
         == 0
