388768c99dafb9b7bf4ae54e9d4b256aac93160b
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2022-12-28 13:59:22 +0000

    Allow 24:00 as charge completion time.

--- tests/test_coordinator_late_ready.py
+++ tests/test_coordinator_late_ready.py
@@ -1,4 +1,5 @@
 """Test ev_smart_charging coordinator."""
+from datetime import datetime
 
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
@@ -6,6 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.const import STATE_ON, STATE_OFF
 from homeassistant.helpers.entity_registry import async_get as async_entity_registry_get
 from homeassistant.helpers.entity_registry import EntityRegistry
+from homeassistant.util import dt as dt_util
 
 from custom_components.ev_smart_charging.coordinator import (
     EVSmartChargingCoordinator,
@@ -20,7 +22,7 @@ from tests.helpers.helpers import (
     MockTargetSOCEntity,
 )
 from tests.price import PRICE_20220930, PRICE_20221001, PRICE_20221002
-from .const import MOCK_CONFIG_LATE
+from .const import MOCK_CONFIG_LATE, MOCK_CONFIG_LATE24
 
 # pylint: disable=unused-argument
 async def test_coordinator_late_ready(
@@ -63,6 +65,13 @@ async def test_coordinator_late_ready(
 
     assert coordinator.auto_charging_state == STATE_OFF
     assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 9, 30, 12, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 9, 30, 17, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 5
 
     # Move time to scheduled charging time
     freezer.move_to("2022-09-30T12:30:00+02:00")
@@ -100,6 +109,13 @@ async def test_coordinator_late_ready(
     assert coordinator.auto_charging_state == STATE_OFF
     assert coordinator.sensor.state == STATE_OFF
     assert coordinator.sensor.charging_is_planned is True
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 10, 1, 3, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 10, 1, 8, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 5
 
     # Move time to after midnight
     freezer.move_to("2022-10-01T00:30:00+02:00")
@@ -136,6 +152,13 @@ async def test_coordinator_late_ready(
     assert coordinator.auto_charging_state == STATE_OFF
     assert coordinator.sensor.state == STATE_OFF
     assert coordinator.sensor.charging_is_planned is True
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 10, 1, 12, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 10, 1, 14, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 2
 
     # Move time to scheduled charging time
     freezer.move_to("2022-10-01T12:30:00+02:00")
@@ -171,3 +194,144 @@ async def test_coordinator_late_ready(
     assert coordinator.auto_charging_state == STATE_OFF
     assert coordinator.sensor.state == STATE_OFF
     assert coordinator.sensor.charging_is_planned is True
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 10, 2, 5, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 10, 2, 7, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 2
+
+
+async def test_coordinator_late_ready2(
+    hass: HomeAssistant, skip_service_calls, set_cet_timezone, freezer
+):
+    """Test Coordinator."""
+
+    freezer.move_to("2022-09-30T10:00:00+02:00")
+
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    MockSOCEntity.create(hass, entity_registry, "55")
+    MockTargetSOCEntity.create(hass, entity_registry, "80")
+    MockPriceEntity.create(hass, entity_registry, 123)
+    MockChargerEntity.create(hass, entity_registry, STATE_OFF)
+    MockPriceEntity.set_state(hass, PRICE_20220930, None)
+
+    # Ready by 24:00
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_LATE24, entry_id="test"
+    )
+    coordinator = EVSmartChargingCoordinator(hass, config_entry)
+    assert coordinator is not None
+
+    sensor: EVSmartChargingSensor = EVSmartChargingSensor(config_entry)
+    assert sensor is not None
+    await coordinator.add_sensor(sensor)
+
+    # Provide price. This should give a 5h schedule, 19:00-24:00 schedule
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid is False
+
+    # Turn on switches
+    await coordinator.switch_active_update(True)
+    await coordinator.switch_apply_limit_update(False)
+    await coordinator.switch_continuous_update(True)
+    await coordinator.switch_ev_connected_update(True)
+    await coordinator.switch_keep_on_update(False)
+    await hass.async_block_till_done()
+
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is True
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 9, 30, 19, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 10, 1, 0, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 5
+
+    # Move time to after new price is available
+    freezer.move_to("2022-09-30T13:30:00+02:00")
+    MockPriceEntity.set_state(hass, PRICE_20220930, PRICE_20221001)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is True
+
+    # Move time to scheduled charging time
+    freezer.move_to("2022-09-30T19:30:00+02:00")
+    await coordinator.update_state()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_ON
+    assert coordinator.sensor.state == STATE_ON
+
+
+async def test_coordinator_late_ready3(
+    hass: HomeAssistant, skip_service_calls, set_cet_timezone, freezer
+):
+    """Test Coordinator."""
+
+    freezer.move_to("2022-09-30T14:00:00+02:00")
+
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    MockSOCEntity.create(hass, entity_registry, "55")
+    MockTargetSOCEntity.create(hass, entity_registry, "80")
+    MockPriceEntity.create(hass, entity_registry, 123)
+    MockChargerEntity.create(hass, entity_registry, STATE_OFF)
+    MockPriceEntity.set_state(hass, PRICE_20220930, PRICE_20221001)
+
+    # Ready by 24:00
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_LATE24, entry_id="test"
+    )
+    coordinator = EVSmartChargingCoordinator(hass, config_entry)
+    assert coordinator is not None
+
+    sensor: EVSmartChargingSensor = EVSmartChargingSensor(config_entry)
+    assert sensor is not None
+    await coordinator.add_sensor(sensor)
+
+    # Provide price.
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid is True
+
+    # Turn on switches
+    await coordinator.switch_active_update(True)
+    await coordinator.switch_apply_limit_update(False)
+    await coordinator.switch_continuous_update(True)
+    await coordinator.switch_ev_connected_update(True)
+    await coordinator.switch_keep_on_update(False)
+    await hass.async_block_till_done()
+
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is False
+    # TODO: Would it be better if a schedule is made here?
+
+    # Move time to after midnight
+    # This should give a 5h schedule, 03:00-08:00 schedule
+    freezer.move_to("2022-10-01T00:30:00+02:00")
+    MockPriceEntity.set_state(hass, PRICE_20221001, None)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is True
+    assert coordinator.sensor.charging_start_time == datetime(
+        2022, 10, 1, 3, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_stop_time == datetime(
+        2022, 10, 1, 8, 0, tzinfo=dt_util.get_time_zone("Europe/Stockholm")
+    )
+    assert coordinator.sensor.charging_number_of_hours == 5
+
+    # Move time to scheduled charging time
+    freezer.move_to("2022-10-01T03:00:00+02:00")
+    await coordinator.update_state()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_ON
+    assert coordinator.sensor.state == STATE_ON
