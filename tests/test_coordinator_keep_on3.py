7004afdebd1aa8c5eddf1baf3b5e76de3440e6f1
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2022-12-29 14:36:33 +0000

    Fixed issue when keep_on is used and EV reconnects

---
+++ tests/test_coordinator_keep_on3.py
@@ -0,0 +1,108 @@
+"""Test ev_smart_charging coordinator."""
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from homeassistant.core import HomeAssistant
+from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.helpers.entity_registry import async_get as async_entity_registry_get
+from homeassistant.helpers.entity_registry import EntityRegistry
+
+from custom_components.ev_smart_charging import async_setup_entry
+from custom_components.ev_smart_charging.coordinator import (
+    EVSmartChargingCoordinator,
+)
+from custom_components.ev_smart_charging.const import DOMAIN
+
+from tests.helpers.helpers import (
+    MockChargerEntity,
+    MockPriceEntity,
+    MockSOCEntity,
+    MockTargetSOCEntity,
+)
+from tests.price import PRICE_20220930, PRICE_20221001
+from .const import (
+    MOCK_CONFIG_KEEP_ON_ISSUE,
+)
+
+# pylint: disable=unused-argument
+async def test_coordinator_keep_on_issue(
+    hass: HomeAssistant, set_cet_timezone, freezer
+):
+    """Test Coordinator."""
+
+    # Test a specific case with keep_on, where the EV is disconnected before
+    # the "Charge completion time", used for a while and then reconnected
+    # to the charger after the "Charge completion time" but before tomorrow's
+    # prices are available.
+
+    freezer.move_to("2022-09-30T14:00:00+02:00")
+
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    MockSOCEntity.create(hass, entity_registry, "80")
+    MockTargetSOCEntity.create(hass, entity_registry, "80")
+    MockPriceEntity.create(hass, entity_registry, 123)
+    MockChargerEntity.create(hass, entity_registry, STATE_OFF)
+
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_KEEP_ON_ISSUE, entry_id="test"
+    )
+    assert await async_setup_entry(hass, config_entry)
+    await hass.async_block_till_done()
+    assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
+    assert isinstance(
+        hass.data[DOMAIN][config_entry.entry_id], EVSmartChargingCoordinator
+    )
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    # coordinator = EVSmartChargingCoordinator(hass, config_entry)
+    assert coordinator is not None
+
+    # Provide price
+    MockPriceEntity.set_state(hass, PRICE_20220930, PRICE_20221001)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid
+
+    # Turn on switches
+    await coordinator.switch_active_update(True)
+    await coordinator.switch_apply_limit_update(False)
+    await coordinator.switch_continuous_update(True)
+    await coordinator.switch_ev_connected_update(True)
+    await coordinator.switch_keep_on_update(True)
+    await hass.async_block_till_done()
+
+    assert coordinator.auto_charging_state == STATE_ON
+    assert coordinator.sensor.state == STATE_ON
+    assert coordinator.sensor.charging_is_planned is False
+
+    # Move time to next day
+    freezer.move_to("2022-10-01T02:00:00+02:00")
+    MockPriceEntity.set_state(hass, PRICE_20221001, None)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_ON
+    assert coordinator.sensor.state == STATE_ON
+    assert coordinator.sensor.charging_is_planned is False
+
+    # Disconnect
+    await coordinator.switch_ev_connected_update(False)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is False
+
+    # Drive a little
+    MockSOCEntity.set_state(hass, "78")
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
+    assert coordinator.sensor.charging_is_planned is True
+
+    # Move time to 12:20, and connect
+    freezer.move_to("2022-10-01T12:20:00+02:00")
+    await coordinator.switch_ev_connected_update(True)
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.auto_charging_state == STATE_OFF
+    assert coordinator.sensor.state == STATE_OFF
