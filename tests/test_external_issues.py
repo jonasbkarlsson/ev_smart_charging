39e3770830eb778598d6525c69f72f6a9030a51d
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2023-01-07 21:23:45 +0000

    Added test for nordpool bug.

---
+++ tests/test_external_issues.py
@@ -0,0 +1,55 @@
+"""Test ev_smart_charging external issues."""
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from homeassistant.core import HomeAssistant
+from homeassistant.const import STATE_OFF
+from homeassistant.helpers.entity_registry import async_get as async_entity_registry_get
+from homeassistant.helpers.entity_registry import EntityRegistry
+
+from custom_components.ev_smart_charging.coordinator import (
+    EVSmartChargingCoordinator,
+)
+from custom_components.ev_smart_charging.const import DOMAIN
+from custom_components.ev_smart_charging.sensor import EVSmartChargingSensor
+
+from tests.helpers.helpers import (
+    MockChargerEntity,
+    MockPriceEntity,
+    MockSOCEntity,
+    MockTargetSOCEntity,
+)
+from tests.price import PRICE_20221001
+from tests.const import MOCK_CONFIG_ALL
+
+# pylint: disable=unused-argument
+async def test_external_issue_nordpool_235(
+    hass: HomeAssistant, skip_service_calls, set_cet_timezone, freezer
+):
+    """Test Coordinator."""
+
+    # Fix to take care of Nordpool bug
+    # https://github.com/custom-components/nordpool/issues/235
+
+    freezer.move_to("2022-10-01T03:00:00+02:00")
+
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    MockSOCEntity.create(hass, entity_registry, "55")
+    MockTargetSOCEntity.create(hass, entity_registry, "80")
+    MockPriceEntity.create(hass, entity_registry, 123)
+    MockChargerEntity.create(hass, entity_registry, STATE_OFF)
+
+    # Set the price the same for today and tomorrow
+    MockPriceEntity.set_state(hass, PRICE_20221001, PRICE_20221001)
+
+    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_ALL, entry_id="test")
+    coordinator = EVSmartChargingCoordinator(hass, config_entry)
+    assert coordinator is not None
+
+    sensor: EVSmartChargingSensor = EVSmartChargingSensor(config_entry)
+    assert sensor is not None
+    await coordinator.add_sensor(sensor)
+
+    await coordinator.update_sensors()
+    await hass.async_block_till_done()
+    assert coordinator.tomorrow_valid is False
