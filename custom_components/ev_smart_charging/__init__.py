4bdb75af05bad40ea1033c3148c1e92f778e9c22
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2023-03-12 15:48:27 +0000

    Integration name and Device name are synchronized.

--- custom_components/ev_smart_charging/__init__.py
+++ custom_components/ev_smart_charging/__init__.py
@@ -3,20 +3,19 @@
 import asyncio
 import logging
 
-from homeassistant.config_entries import (
-    ConfigEntry,
-    ConfigEntryChange,
-    SIGNAL_CONFIG_ENTRY_CHANGED,
-)
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.dispatcher import async_dispatcher_send
-
-from custom_components.ev_smart_charging.helpers.general import get_parameter
+from homeassistant.helpers.device_registry import async_get as async_device_registry_get
+from homeassistant.helpers.device_registry import DeviceRegistry, DeviceEntry
+from homeassistant.helpers.entity_registry import async_get as async_entity_registry_get
+from homeassistant.helpers.entity_registry import (
+    EntityRegistry,
+    async_entries_for_config_entry,
+)
 
 from .coordinator import EVSmartChargingCoordinator
 from .const import (
-    CONF_DEVICE_NAME,
     DOMAIN,
     STARTUP_MESSAGE,
     PLATFORMS,
@@ -39,20 +38,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.data.setdefault(DOMAIN, {})
         _LOGGER.debug(STARTUP_MESSAGE)
 
-    # Make sure the Integration name is the same as the Device name
-    # This is currently needed since
-    # homeassistant.config_entries.OptionsFlowManager.async_finish_flow()
-    # does not pass "title" to self.hass.config_entries.async_update_entry()
-    # Don't bother to test code copied from async_update_entry()
-    if entry.title != get_parameter(entry, CONF_DEVICE_NAME):
-        entry.title = get_parameter(entry, CONF_DEVICE_NAME)
-        for listener_ref in entry.update_listeners:
-            if (listener := listener_ref()) is not None:  # pragma: no cover
-                hass.async_create_task(listener(hass, entry))  # pragma: no cover
-        async_dispatcher_send(
-            hass, SIGNAL_CONFIG_ENTRY_CHANGED, ConfigEntryChange.UPDATED, entry
-        )
-
     coordinator = EVSmartChargingCoordinator(hass, entry)
     validation_error = coordinator.validate_input_sensors()
     if validation_error is not None:
@@ -71,6 +56,26 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             )
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
+    # If the name of the integration (config_entry.title) has changed,
+    # update the device name.
+    entity_registry: EntityRegistry = async_entity_registry_get(hass)
+    all_entities = async_entries_for_config_entry(entity_registry, entry.entry_id)
+    if all_entities:
+        device_id = all_entities[0].device_id
+        device_registry: DeviceRegistry = async_device_registry_get(hass)
+        device: DeviceEntry = device_registry.async_get(device_id)
+        if device:
+            if device.name_by_user is not None:
+                if entry.title != device.name_by_user:
+                    device_registry.async_update_device(
+                        device.id, name_by_user=entry.title
+                    )
+            else:
+                if entry.title != device.name:
+                    device_registry.async_update_device(
+                        device.id, name_by_user=entry.title
+                    )
 
     return True
 
