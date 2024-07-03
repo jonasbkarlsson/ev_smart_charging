2165d0217c9ed920abcf48ab545f9912105e2a75
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2024-07-03 11:53:18 +0200

    Fix config entry setup warning

--- custom_components/ev_smart_charging/__init__.py
+++ custom_components/ev_smart_charging/__init__.py
@@ -47,12 +47,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
