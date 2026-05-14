b712c16af458025256fe369aa1686883a9e92091
Nika Gerson Lohman <nikagl@gmail.com>
2026-05-14 11:18:34 +0200

    Fix manifest update key ordering for hassfest

--- scripts/update_manifest.py
+++ scripts/update_manifest.py
@@ -19,10 +19,20 @@ def update_manifest():
 
     manifest["version"] = version
 
+    ordered_manifest = {}
+    if "domain" in manifest:
+        ordered_manifest["domain"] = manifest["domain"]
+    if "name" in manifest:
+        ordered_manifest["name"] = manifest["name"]
+
+    for key in sorted(key for key in manifest if key not in {"domain", "name"}):
+        ordered_manifest[key] = manifest[key]
+
     with open(
         f"{os.getcwd()}/custom_components/ev_smart_charging/manifest.json", "w"
     ) as manifestfile:
-        manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
+        manifestfile.write(json.dumps(ordered_manifest, indent=2))
+        manifestfile.write("\n")
 
 
 update_manifest()
